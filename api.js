93e2a2d29edb3faee3b5b0350222cdc1858ac644
Adithya Vijay K <adithyavj@gmail.com>
2022-01-30 23:50:03 +0530

    Add API config

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 var db = require('./dboperations');
 var order = require('./order');
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);
+
 db.getOrders().then(result => {
     console.log(result);
 })
